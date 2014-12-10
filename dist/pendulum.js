17c3d629aa0a8d30b328ee9ffb9cd487add057ad
Eldad Bercovici <dutzi.b@gmail.com>
2014-12-10 08:54:21 +0200

    Moved message up

--- dist/pendulum.js
+++ dist/pendulum.js
@@ -1,5 +1,11 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Pendulum=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Pendulum(options) {
+	var ieOption = 'dontDisplayHorrendousMessageBoxOnIE8EvenThoughItsAShitty' + 
+		'BrowserNobodyShouldNeverEverUse';
+	if (navigator.userAgent.match(/MSIE 8/i) && options[ieOption] !== true) {
+		alert('Please consider upgrading your browser. It\'s really old.');
+	}
+
 	this.options		= options;
 	this.canvas			= document.createElement('canvas');
 	this.fromColor		= this.parseColor(options.color1);
@@ -25,12 +31,6 @@ function Pendulum(options) {
 	this.context.scale(pixelRatio, pixelRatio);
 
 	this.play();
-
-	var ieOption = 'dontDisplayHorrendousMessageBoxOnIE8EvenThoughItsAShitty' + 
-		'BrowserNobodyShouldNeverEverUse';
-	if (navigator.userAgent.match(/MSIE 8/i) && options[ieOption] !== true) {
-		alert('Please consider upgrading your browser. It\'s really old.');
-	}
 }
 
 Pendulum.prototype.play = function() {
